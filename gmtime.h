2d14569ebb4f5b802f5df6295c905ae011fed655
Michael G. Schwern <schwern@pobox.com>
2008-08-26 00:59:01 +0000

    Elminate all the pivot stuff, we don't need it for Perl.
    git-svn-id: https://y2038.googlecode.com/svn@3 95f99ffb-5155-0410-b9a3-cdf7dd7c4864

--- gmtime.h
+++ gmtime.h
@@ -1,3 +1,7 @@
+/* This is a modification of code produced Paul Sheer from 2038bug.com.
+   His copyright, conditions and disclaimer are reproduced below.
+*/
+
 /* pivotal_gmtime_r - a replacement for gmtime/localtime/mktime
                       that works around the 2038 bug on 32-bit
                       systems. (Version 3)
@@ -35,37 +39,7 @@
 
 */
 
-const char pivotal_gmtime_r_stamp[] =
-    "pivotal_gmtime_r. Copyright (C) 2005  Paul Sheer. Terms and "
-    "conditions apply. Visit http://2038bug.com/ for more info.";
-
 /*
-
-On 32-bit machines, with 'now' passed as NULL, pivotal_gmtime_r() gives
-the same result as gmtime() (i.e. gmtime() of the GNU C library 2.2) for
-all values of time_t positive and negative. See the gmtime() man page
-for more info.
-
-It is intended that you pass 'now' as the current time (as previously
-retrieved with a call such as time(&now);). In this case,
-pivotal_gmtime_r() returns the correct broken down time in the range of
-    now - 2147483648 seconds
-through to
-    now + 2147483647 seconds
-
-For example, on 10-Jan-2008, pivotal_gmtime_r() will return the correct
-broken down time format from 23-Dec-1939 through 29-Jan-2076.
-
-For all values of 'now' before Jan-23-2005 and after Jan-19-2038,
-pivotal_gmtime_r() will return the correct broken down time format from
-exactly 01-Jan-1970 through to 07-Feb-2106.
-
-In other words, if, for example, pivotal_gmtime_r() is used in a program
-that needs to convert time values of 25 years into the future and 68
-years in the past, the program will operate as expected until the year
-2106-25=2081. This will be true even on 32-bit systems.
-
-Note that "Jan-23-2005" is the date of the authoring of this code.
 
 Programmers who have available to them 64-bit time values as a 'long
 long' type can use gmtime64_r() instead, which correctly converts the
@@ -75,58 +49,9 @@ will depend on the operating system.
 Both functions are 64-bit clean and should work as expected on 64-bit
 systems. They have not yet been tested on 64-bit systems however.
 
-The localtime() equivalent functions do both a POSIX localtime_r() and
-gmtime_r() and work out the time zone offset from their difference. This
-is inefficient but gives the correct timezone offset and daylight
-savings time adjustments.
-
-The function prototypes are:
-
-    long long pivot_time_t (const time_t * now, long long *t);
-    long long mktime64 (struct tm *t);
-    struct tm *localtime64_r (const long long *t, struct tm *p);
-    struct tm *pivotal_localtime_r (const time_t * now, const time_t * t, struct tm *p);
-    struct tm *gmtime64_r (const long long *t, struct tm *p);
-    struct tm *pivotal_gmtime_r (const time_t * now, const time_t * t, struct tm *p);
-
-pivot_time_t() takes a 64-bit time that may have had its top 32-bits set
-to zero, and adjusts it so that it is in the range explained above. You
-can use pivot_time_t() to convert any time that may be incorrect.
-pivot_time_t() returns its argument unchanged if either now is NULL or
-sizeof(time_t) is not 4.
-
-mktime64() is a 64-bit equivalent of mktime().
-
 localtime64_r() is a 64-bit equivalent of localtime_r().
 
-pivotal_localtime_r() is 32-bit equivalent of localtime_r() with
-pivoting.
-
 gmtime64_r() is a 64-bit equivalent of gmtime_r().
-
-pivotal_gmtime_r() is a 32-bit equivalent of gmtime_r() with pivoting.
-
-Note that none of these functions handle leap seconds.
-
-RATIONALE: The purpose of pivotal_gmtime_r() is as a replacement for the
-functions gmtime(), localtime() and their corresponding reentrant
-versions gmtime_r() and localtime_r().
-
-pivotal_gmtime_r() is meant for 32-bit systems that must still correctly
-convert 32-bit time into broken down time format through the year 2038.
-Since most programs tend to operate within a range of time no more than
-68 years in the future or the past, it is possible to determine the
-correct interpretation of a 32-bit time value in spite of the wrap that
-occurs in the year 2038.
-
-Many databases are likely to store time in 32-bit format and not be
-easily upgradeable to 64-bit. By using pivot_time_t(), these time values
-can be correctly used.
-
-
-Changes:
-    06-Feb-2005  v3.0:  Some optimizations.
-			mktime() no-longer zeros tm struct.
 
 */
 
@@ -200,40 +125,24 @@ int _safe_year(int year)
     return safe_year;
 }
 
-long long pivot_time_t (const time_t * now, long long *_t)
-{
-    long long t;
-    t = *_t;
-    if (now && sizeof (time_t) == 4) {
-        time_t _now;
-        _now = *now;
-        if (_now < 1106500000 /* Jan 23 2005 - date of writing */ )
-            _now = 2147483647;
-        if ((long long) t + ((long long) 1 << 31) < (long long) _now)
-            t += (long long) 1 << 32;
-    }
-    return t;
-}
-
-static struct tm *_gmtime64_r (const time_t * now, long long *_t, struct tm *p)
+struct tm *gmtime64_r (const long long *in_time, struct tm *p)
 {
     int v_tm_sec, v_tm_min, v_tm_hour, v_tm_mon, v_tm_wday, v_tm_tday;
     int leap;
-    long long t;
     long m;
+    long long time = *in_time;
     
     p->tm_gmtoff = 0;
     p->tm_isdst  = 0;
     p->tm_zone   = "UTC";
     
-    t = pivot_time_t (now, _t);
-    v_tm_sec = ((long long) t % (long long) 60);
-    t /= 60;
-    v_tm_min = ((long long) t % (long long) 60);
-    t /= 60;
-    v_tm_hour = ((long long) t % (long long) 24);
-    t /= 24;
-    v_tm_tday = t;
+    v_tm_sec =  time % 60;
+    time /= 60;
+    v_tm_min =  time % 60;
+    time /= 60;
+    v_tm_hour = time % 24;
+    time /= 24;
+    v_tm_tday = time;
     WRAP (v_tm_sec, v_tm_min, 60);
     WRAP (v_tm_min, v_tm_hour, 60);
     WRAP (v_tm_hour, v_tm_tday, 24);
@@ -278,28 +187,22 @@ static struct tm *_gmtime64_r (const time_t * now, long long *_t, struct tm *p)
     return p;
 }
 
-struct tm *gmtime64_r (const long long *_t, struct tm *p)
-{
-    long long t;
-    t = *_t;
-    return _gmtime64_r (NULL, &t, p);
-}
 
-static struct tm *_localtime64_r (const time_t * now, long long *_t, struct tm *local_tm)
+struct tm *localtime64_r (const long long *time, struct tm *local_tm)
 {
-    time_t t;
+    time_t safe_time;
     struct tm gm_tm;
     int orig_year;
     int month_diff;
 
-    _gmtime64_r(now, _t, &gm_tm);
+    gmtime64_r(time, &gm_tm);
     orig_year = gm_tm.tm_year;
 
     if (gm_tm.tm_year > (2037 - 1900))
         gm_tm.tm_year = _safe_year(gm_tm.tm_year + 1900) - 1900;
 
-    t = timegm(&gm_tm);
-    localtime_r(&t, local_tm);
+    safe_time = timegm(&gm_tm);
+    localtime_r(&safe_time, local_tm);
 
     local_tm->tm_year = orig_year;
     month_diff = local_tm->tm_mon - gm_tm.tm_mon;
@@ -313,11 +216,4 @@ static struct tm *_localtime64_r (const time_t * now, long long *_t, struct tm *
     _check_tm(local_tm);
     
     return local_tm;
-}
-
-struct tm *localtime64_r (const long long *_t, struct tm *p)
-{
-    long long tl;
-    tl = *_t;
-    return _localtime64_r (NULL, &tl, p);
 }
