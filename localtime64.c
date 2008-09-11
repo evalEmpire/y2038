e0ad44e69b5605054fd6427cc0e17b3c1f47b636
Michael G. Schwern <schwern@pobox.com>
2008-09-11 10:53:27 +0000

    r60997@windhund:  schwern | 2008-09-11 03:51:57 -0700  Clarifying the license.  I feel this work has diverged sufficiently from  Paul's that I can remove Paul's license and copyright.  I'll contact him  to let him know.
     Removing Paul's pivotal_gmtime_r.c from the distribution since it's under
     a different license.

--- localtime64.c
+++ localtime64.c
@@ -1,46 +1,30 @@
-/* localtime64.h is Copyright (C) 2007-2008  Michael G Schwern
+/* 
 
-   It is a modification on and improvement of code written by 
-   Paul Sheer from 2038bug.com.  His copyright, conditions and 
-   disclaimer are reproduced below.
-*/
+Copyright (c) 2007-2008  Michael G Schwern
 
-/* Copyright (C) 2005  Paul Sheer
+This software originally derived from Paul Sheer's pivotal_gmtime_r.c.
 
-   Redistribution and use in source form, with or without modification,
-   is permitted provided that the above copyright notice, this list of
-   conditions, the following disclaimer, and the following char array
-   are retained.
+The MIT License:
 
-   Redistribution and use in binary form must reproduce an
-   acknowledgment: 'With software provided by http://2038bug.com/' in
-   the documentation and/or other materials provided with the
-   distribution, and wherever such acknowledgments are usually
-   accessible in Your program.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-   This software is provided "AS IS" and WITHOUT WARRANTY, either
-   express or implied, including, without limitation, the warranties of
-   NON-INFRINGEMENT, MERCHANTABILITY or FITNESS FOR A PARTICULAR
-   PURPOSE. THE ENTIRE RISK AS TO THE QUALITY OF THIS SOFTWARE IS WITH
-   YOU. Under no circumstances and under no legal theory, whether in
-   tort (including negligence), contract, or otherwise, shall the
-   copyright owners be liable for any direct, indirect, special,
-   incidental, or consequential damages of any character arising as a
-   result of the use of this software including, without limitation,
-   damages for loss of goodwill, work stoppage, computer failure or
-   malfunction, or any and all other commercial damages or losses. This
-   limitation of liability shall not apply to liability for death or
-   personal injury resulting from copyright owners' negligence to the
-   extent applicable law prohibits such limitation. Some jurisdictions
-   do not allow the exclusion or limitation of incidental or
-   consequential damages, so this exclusion and limitation may not apply
-   to You.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 
 */
-
-const char pivotal_gmtime_r_stamp[] =
-    "pivotal_gmtime_r. Copyright (C) 2005  Paul Sheer. Terms and "
-    "conditions apply. Visit http://2038bug.com/ for more info.";
 
 /*
 
