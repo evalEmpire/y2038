80c941e089b43ef65c82cda4821cc7809a055154
Michael G. Schwern <schwern@pobox.com>
2008-09-14 22:26:30 +0000

    r61035@windhund:  schwern | 2008-09-14 15:25:33 -0700  Fix localtime64's handling of over and underflow.

--- t/overflow.c
+++ t/overflow.c
@@ -3,7 +3,7 @@
 #include "tap.c"
 
 int main(void) {
-    Time64_T time = 0x0FFFFFFFFFFFFFFFLL;;
+    Time64_T time = 0x0FFFFFFFFFFFFFFFLL;
     struct tm date;
     struct tm *error;
 
@@ -13,10 +13,27 @@ int main(void) {
     if( sizeof(date.tm_year) > 4 )
         skip_all( "tm_year is too large to overflow (yay!)" );
 
+    /* Overflow */
     error = gmtime64_r(&time, &date);
-
     ok( error == NULL,          "gmtime64_r() returned null on overflow" );
     is_int( errno, EOVERFLOW,   "  errno set to EOVERFLOW" );
+
+    error = localtime64_r(&time, &date);
+    ok( error == NULL,          "localtime64_r() returned null on overflow" );
+    is_int( errno, EOVERFLOW,   "  errno set to EOVERFLOW" );
+
+
+    /* Underflow */
+    time = -0x0FFFFFFFFFFFFFFFLL;
+
+    error = gmtime64_r(&time, &date);
+    ok( error == NULL,          "gmtime64_r() returned null on underflow" );
+    is_int( errno, EOVERFLOW,   "  errno set to EOVERFLOW" );
+
+    error = localtime64_r(&time, &date);
+    ok( error == NULL,          "localtime64_r() returned null on underflow" );
+    is_int( errno, EOVERFLOW,   "  errno set to EOVERFLOW" );
+
 
     done_testing();
 #endif
