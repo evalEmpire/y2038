82d38ca2032965a5ce5fef2786a597b0a653dad1
Michael G. Schwern <schwern@pobox.com>
2008-09-30 15:45:06 +0000

    r67122@windhund:  schwern | 2008-09-30 11:29:28 -0400  More accurate binary search.

--- bin/check_max.c
+++ bin/check_max.c
@@ -2,6 +2,7 @@
 
 #include <time.h>
 #include <stdio.h>
+#include <math.h>
 
 time_t Time_Zero = 0;
 
@@ -12,13 +13,14 @@ void check_gmtime_max (void)
     /* Let's not bother checking anything smaller than 2**30-1 */
     time_t time = 1073741823;
     time_t last_time = 0;
+    time_t time_change;
     int i;
 
     for (i = 30; i <= 63; i++) {
         date = gmtime(&time);
 
         /* gmtime() broke or tm_year overflowed */
-        if(date == NULL || date->tm_year < 70)
+        if(date == NULL || date->tm_year < 69)
           break;
 
         last_time = time;
@@ -29,6 +31,25 @@ void check_gmtime_max (void)
             break;
     }
 
+    /* Binary search for the exact failure point */
+    time = last_time;
+    time_change = last_time / 2;
+
+    do {
+        time += time_change;
+
+        date = gmtime(&time);
+
+        /* gmtime() broke or tm_year overflowed or time_t overflowed */
+        if(date == NULL || date->tm_year < 69 || time < last_time) {
+            time = last_time;
+            time_change = time_change / 2;
+        }
+        else {
+            last_time = time;
+        }
+    } while(time_change > 0);
+
     printf("%20s %.0f\n", "gmtime max", difftime(last_time, Time_Zero));
 }
 
@@ -38,13 +59,14 @@ void check_gmtime_min (void)
     struct tm *date;
     time_t time = -1;
     time_t last_time = 0;
+    time_t time_change;
     int i;
 
     for (i = 1; i <= 63; i++) {
         date = gmtime(&time);
 
         /* gmtime() broke or tm_year underflowed */
-        if(date == NULL || date->tm_year > 69)
+        if(date == NULL || date->tm_year > 70)
             break;
 
         last_time = time;
@@ -54,6 +76,25 @@ void check_gmtime_min (void)
         if( time > last_time )
             break;
     }
+
+    /* Binary search for the exact failure point */
+    time = last_time;
+    time_change = last_time / 2;
+
+    do {
+        time -= time_change;
+
+        date = gmtime(&time);
+
+        /* gmtime() broke or tm_year overflowed or time_t overflowed */
+        if(date == NULL || date->tm_year < 69 || time < last_time) {
+            time = last_time;
+            time_change = time_change / 2;
+        }
+        else {
+            last_time = time;
+        }
+    } while(time_change > 0);
 
     printf("%20s %.0f\n", "gmtime min", difftime(last_time, Time_Zero));
 }
