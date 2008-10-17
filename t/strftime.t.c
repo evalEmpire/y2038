b5b815a19f61514c7632c4512cce87dfa9afd0d1
Michael G. Schwern <schwern@pobox.com>
2008-10-17 02:34:07 +0000

    Write a function to make testing strftime64() easier.
    Signed-off-by: Michael G. Schwern <schwern@pobox.com>

--- t/strftime.t.c
+++ t/strftime.t.c
@@ -2,24 +2,26 @@
 #include <time64.h>
 #include "t/tap.c"
 
+void strftime64_ok(struct TM *date, const char *format, char *expect) {
+    char      buffer[1000];
+    size_t    size;
+
+    size = strftime64(buffer, 1000, format, date);
+    is_int( (int)size, (int)strlen(buffer), "returned right size" );
+    is_str( buffer, expect, format );
+}
+
 int main(void) {
     struct TM date;
     Time64_T  time;
-    char      buffer[10000];
-    int       size;
 
     setlocale(LC_ALL, "");
 
     time = 60 * 60 * 24 * 15;  /* Jan 15th, 1970 ish */
     localtime64_r(&time, &date);
 
-    size = strftime64(buffer, 10000, "%Y", &date);
-    is_int( size, (int)strlen(buffer), "returned right size" );
-    is_str( buffer, "1970", "%%Y" );
-
-    size = strftime64(buffer, 10000, "%m", &date);
-    is_int( size, (int)strlen(buffer), "returned right size" );
-    is_str( buffer, "01", "%%m" );
+    strftime64_ok(&date, "%Y", "1970");
+    strftime64_ok(&date, "%m", "01" );
 
     done_testing();
 
