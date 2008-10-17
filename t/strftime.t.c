24406be60d6195710d1039d2c6ac19f932b0c00c
Michael G. Schwern <schwern@pobox.com>
2008-10-17 02:58:30 +0000

    Include a test constructed based on the current date.
    Signed-off-by: Michael G. Schwern <schwern@pobox.com>

--- t/strftime.t.c
+++ t/strftime.t.c
@@ -1,5 +1,6 @@
 #include <locale.h>
 #include <time64.h>
+#include <stdio.h>
 #include "t/tap.c"
 
 void strftime64_ok(struct TM *date, const char *format, char *expect) {
@@ -8,12 +9,15 @@ void strftime64_ok(struct TM *date, const char *format, char *expect) {
 
     size = strftime64(buffer, 1000, format, date);
     is_int( (int)size, (int)strlen(buffer), "returned right size" );
+
+    /* XXX It would be nice if the %'s in format were escaped */
     is_str( buffer, expect, format );
 }
 
 int main(void) {
     struct TM date;
     Time64_T  time;
+    char expected[1000];
 
     setlocale(LC_ALL, "");
 
@@ -22,6 +26,9 @@ int main(void) {
 
     strftime64_ok(&date, "%Y", "1970");
     strftime64_ok(&date, "%m", "01" );
+
+    sprintf(expected, "%02d/%02d/%0lld", date.tm_mon+1, date.tm_mday, (Year)date.tm_year + 1900);
+    strftime64_ok(&date, "%m/%d/%Y", expected);
 
     done_testing();
 
