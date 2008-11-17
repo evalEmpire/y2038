0be25807631ac00c7f837ac57b39f7a12245d0bd
Michael G. Schwern <schwern@pobox.com>
2008-11-17 22:24:02 +0000

    Fix Cygwin so the check_max program is not always run.

--- perl/Time-y2038/inc/Local/Module/Build.pm
+++ perl/Time-y2038/inc/Local/Module/Build.pm
@@ -7,15 +7,16 @@ sub note_time_limits {
     my $self = shift;
 
     my $source = "check_max.c";
-    my $exe    = "check_max";
+    my $exe    = $self->notes("check_max") || "check_max";
     unless( $self->up_to_date($source => $exe) ) {
         warn "Building a program to test the range of your system time functions...\n";
         my $cb = $self->cbuilder;
         my $obj = $cb->compile(source => "check_max.c", include_dirs => ['y2038']);
         $exe = $cb->link_executable(objects => $obj, exe_file => $exe);
+	$exe = $self->find_real_exe($exe);
+	$self->notes(check_max => $exe);
+	$self->add_to_cleanup($exe);
     }
-
-    $self->add_to_cleanup($exe);
 
     return if $self->up_to_date(["munge_config", $exe] => "y2038/time64_config.h");
     warn "  and running it...\n";
@@ -48,6 +49,24 @@ sub note_time_limits {
     }
 
     return;
+}
+
+
+# This is necessary because Cygwin nerotically puts a .exe on
+# every executable.  This appears to be built into gcc.
+sub find_real_exe {
+    my $self = shift;
+    my $exe = shift;
+
+    my $real_exe;
+    for ($exe, "$exe.exe") {
+	$real_exe = $_ if -e;
+    }
+
+    warn "Can't find the executable, thought it was $exe"
+	unless defined $real_exe;
+
+    return $real_exe;
 }
 
 
